48b84818b9f1cc1347ce541394d8a400662cd422
unknown <derejetabzaw@gmail.com>
2020-11-26 13:53:34 -0800

    Added audio comparison

---
+++ audio_comparison.py
@@ -0,0 +1,16 @@
+from dejavu import Dejavu
+from dejavu.recognize import FileRecognizer
+import json
+
+
+with open("dejavu.cnf.SQLITE_SAMPLE") as f:
+    config = json.load(f)
+djv = Dejavu(config)
+
+songs = []
+def audio_compare(indexed_audio):
+	print indexed_audio
+	for audio_file in indexed_audio:
+		print audio_file
+		songs.append(djv.recognize(FileRecognizer,audio_file))
+	return songs
